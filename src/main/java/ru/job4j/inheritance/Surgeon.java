b0dfb2b8ba0acb1420650036026b3d28a6866dd1
Kollersan <kollersan@gmail.com>
2022-04-19 17:13:05 +0500

    3. Реализация профессий в коде [#6837 #289991]

--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -2,7 +2,10 @@ package ru.job4j.inheritance;
 
 public class Surgeon extends Doctor {
 
-    public Surgeon(String name, String surname, String education, String birthday, String direction) {
+    private int age;
+
+    public Surgeon(String name, String surname, String education, String birthday, String direction, int age) {
         super(name, surname, education, birthday, direction);
+        this.age = age;
     }
 }
