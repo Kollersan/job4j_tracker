9b5361e0c89d68c202ae32b0732cb509b76b9b58
Kollersan <kollersan@gmail.com>
2022-04-19 17:39:08 +0500

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,14 @@
 package ru.job4j.tracker;
 
-public class StartUI {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime date = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyy HH:mm:ss");
+        String dateformat = date.format(formatter);
+        System.out.println(dateformat);
+    }
 }
