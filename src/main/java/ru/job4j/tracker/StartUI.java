3232f902e2f160479655c86b0bfaaa68b1392c10
Kollersan <kollersan@gmail.com>
2022-04-26 20:23:43 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,14 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime date = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyy HH:mm:ss");
-        String dateformat = date.format(formatter);
-        System.out.println(dateformat);
+        System.out.println(item);
     }
 }
