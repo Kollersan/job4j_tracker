18baf798d55aa9fad482aa0ebe118bb629b099ce
Kollersan <kollersan@gmail.com>
2022-05-31 20:44:22 +0500

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThrows;
 
 public class StartUITest {
 
@@ -113,4 +114,25 @@ public class StartUITest {
         ));
     }
 
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"9", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "-=Menu=-: " + ln
+                        + "0. Exit" + ln
+                        + "Wrong input, you can select: 0 ..0" + ln
+                        + "-=Menu=-: " + ln
+                        + "0. Exit" + ln
+                )
+        );
+    }
 }
